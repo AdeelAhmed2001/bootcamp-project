c6fd2a71bbdc1bb7827798fa38f0dcba84358f78
AdeelAhmed2001 <66421548+AdeelAhmed2001@users.noreply.github.com>
2020-06-12 00:58:53 +0500

    done 7

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 const Display = React.createElement('div',null,'Hello world to all')
 ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
   Display,document.getElementById('root')
 );
 
